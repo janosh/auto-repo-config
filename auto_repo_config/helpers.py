08c89c2556d5bb2523494a7aa5c0948e01ff3f29
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-07-16 08:53:08 +0200

    split into main + helpers module, add argparse CLI

---
+++ auto_repo_config/helpers.py
@@ -0,0 +1,134 @@
+import os
+from typing import Dict, List, Tuple, Union
+
+import requests
+import yaml
+
+
+if os.path.exists("gh_token.py"):
+    from gh_token import GH_TOKEN
+else:
+    GH_TOKEN = os.environ["GH_TOKEN"]
+
+
+headers = {"Authorization": f"token {GH_TOKEN}"}
+
+
+def query_gh_gpl_api(query: str) -> dict:
+    """Query the GitHub GraphQL API.
+
+    Args:
+        query (str): Multi-line query string. Use triple-quotes. Minimal example:
+            '''
+            {
+              viewer {
+                login
+              }
+            }
+            '''
+
+    Raises:
+        Exception: If the query returned an error message.
+
+    Returns:
+        dict: The data returned by the API.
+    """
+    response = requests.post(
+        "https://api.github.com/graphql", json={"query": query}, headers=headers
+    ).json()
+
+    if "errors" in response:
+        err = response["errors"][0]["message"]
+        raise Exception(f"Request failed with error '{err}'.")
+    else:
+        return response["data"]
+
+
+def pretty_print(dic: dict) -> None:
+    """Pretty print a dictionary in YAML format.
+    Useful for development and debugging.
+    """
+    print(yaml.dump(dic))
+
+
+def get_gql_query(settings: str, affil: str = "OWNER") -> str:
+    """Construct GraphQL query from settings list.
+
+    Args:
+        settings (str): Names of repo settings according to the GraphQL API,
+            separated by new lines. Use '\n'.join(settings_list).
+        affil (str, optional): Comma-separated string of author affiliations to their
+            repos. One or several of OWNER, COLLABORATOR, ORGANIZATION_MEMBER.
+            Defaults to "OWNER".
+
+    Returns:
+        str: GraphQL query.
+    """
+    return """{
+      viewer {
+        repositories(first: 100, affiliations: [{affil}]) {
+          nodes {
+            name
+            nameWithOwner
+            isArchived
+            isFork
+            {settings}
+          }
+        }
+        organizations(first: 100) {
+          nodes {
+            login
+            repositories(first: 100) {
+              nodes {
+                name
+                nameWithOwner
+                isArchived
+                isFork
+                {settings}
+              }
+            }
+          }
+        }
+      }
+    }""".replace(
+        "{settings}", settings
+    ).replace(
+        "{affil}", affil
+    )
+
+
+def load_config(
+    config_path: str = None,
+) -> Tuple[Dict[str, Dict[str, Union[str, bool]]], List[str], bool]:
+    """Load .repo-config.(yml|yaml).
+
+    Returns:
+        Tuple[Dict[str, Dict[str, Union[str, bool]]], List[str], bool]: [description]
+    """
+
+    config = {}
+
+    if config_path and not os.path.exists(config_path):
+        raise FileNotFoundError(
+            f"Path to config file was set as '{config_path}' but no such file exists."
+        )
+    elif config_path:
+        with open(config_path) as file:
+            config = yaml.safe_load(file.read())
+
+    for path in (".repo-config.yml", ".repo-config.yaml"):
+        if os.path.exists(path):
+            with open(path) as file:
+                config = yaml.safe_load(file.read())
+
+    if config == {}:
+        raise ValueError(
+            "No config file could be found. See https://git.io/JWa5o for an example "
+            "config file. All fields except 'settings' are optional."
+        )
+
+    settings = config["settings"]
+    orgs = config["orgs"] or []
+    skipForks = config["skipForks"] or True
+
+    return settings, orgs, skipForks
---
+++ auto_repo_config/main.py
@@ -0,0 +1,112 @@
+import os
+from argparse import ArgumentParser
+from importlib.metadata import version
+from typing import Sequence
+
+import requests
+
+from auto_repo_config.helpers import (
+    get_gql_query,
+    load_config,
+    query_gh_gpl_api,
+)
+
+
+if os.path.exists("gh_token.py"):
+    from gh_token import GH_TOKEN
+else:
+    GH_TOKEN = os.environ["GH_TOKEN"]
+
+
+headers = {"Authorization": f"token {GH_TOKEN}"}
+
+
+def main(argv: Sequence[str] = None) -> int:
+
+    parser = ArgumentParser(
+        "auto-repo-config",
+        description="Periodically run a GitHub Action to change settings on your own "
+        "and/or org repos if they differ from those specified in .repo-config.yaml.",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Specify a custom path to your config file.",
+        default=None,
+    )
+
+    arc_version = version("auto-repo-config")
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s v{arc_version}"
+    )
+
+    args = parser.parse_args(argv)
+
+    settings, org_logins, skipForks = load_config(args.config)
+
+    query = get_gql_query("\n".join(settings.keys()))
+
+    result = query_gh_gpl_api(query)
+
+    repos = result["viewer"]["repositories"]["nodes"]
+    orgs = result["viewer"]["organizations"]["nodes"]
+
+    accessible_org_logins = [d["login"] for d in orgs]
+
+    for org_login in org_logins:
+        if org_login not in accessible_org_logins:
+            print(
+                f"Warning: The 'orgs' key in .repo-config.yaml includes '{org_login}' "
+                "but you do not seem to have access to that org. Accessible "
+                f"orgs are {accessible_org_logins}."
+            )
+
+    # We first query for all org repos and then do the filtering based on orgs
+    # specified in repo-config.yaml so that we only need one request to the GQL API.
+    for org in orgs:
+        if org["login"] in org_logins:
+            org_repos = org["repositories"]["nodes"]
+            repos.extend(org_repos)
+
+    count = 0
+
+    for repo in repos:
+        # skip archived repos
+        if repo["isArchived"]:
+            continue
+        # skip repos whose settings already conform to the config
+        if all(dic["value"] == repo[key] for key, dic in settings.items()):
+            continue
+        # skip forked repos if config says so
+        if skipForks and repo["isFork"]:
+            continue
+
+        print(f"processing {repo['nameWithOwner']}... ")
+
+        # send patch request to GitHub REST API to update repo settings
+        requests.patch(
+            f"https://api.github.com/repos/{repo['nameWithOwner']}",
+            # see https://docs.github.com/en/rest/reference/repos
+            # for the names of repo settings
+            json={d["restName"]: d["value"] for d in settings.values()},
+            headers=headers,
+        )
+
+        for key, dic in settings.items():
+            if dic["value"] != repo[key]:
+                print(f"  - changed {key} to {dic['value']}\t")
+
+        print()
+        count += 1
+
+    if count == 0:
+        print("\nNothing to do, all repos already conformed to config.")
+    else:
+        print(f"\nDone! Modified settings on {count:,} repos.")
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
